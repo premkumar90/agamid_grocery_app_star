a56daae7941ba4c941759447e7cd597bb148afe7
Prem Kumar <codeon.prem@gmail.com>
2025-11-22 18:26:22 -0500

    module exercise added

--- my_script.py
+++ my_script.py
@@ -1,10 +1,28 @@
-# my_list = [1,2,"apple",4.5,"tomato",75]
-# a_list = ["these","are","elements"]
-# first_three = my_list[0:3]
-# print(first_three)
-# my_list2 = [0,1,2,3,4,5]
-# print(my_list2[::-1])
+# list_a = [1,"apple",3.5]
+# list_b = ["banana","tomato"]
+# list_a.insert(1,"banana")
+# print(list_a)
+# my_tuple = (10,20,"orange")
+# my_grand_tuple = my_tuple + (30,40)
+# print(my_grand_tuple)
 
-my_shopping_list = ["bread","milk","eggs"]
-extend = my_shopping_list.extend(["spinach","ghee"])
-print(extend)
+movies = ["Inception", "Interstellar","The Prestige", "Dunkirk","The Dark Knight"]
+movies.append("Memento")
+print(movies)
+movies.remove("Dunkirk")
+print(movies)
+
+numbers = [2,3,5,7,11,13]
+print(numbers[4:])
+colors = ["red","blue","green"]
+colors.insert(1,"yellow")
+print(colors)
+colors.append("purple")
+print(colors)
+
+dimensions = (3,5,7)
+print(dimensions[1])
+fruits = ("apple","banana")
+vegetables = ("carrot","lettuce")
+groceries = fruits + vegetables 
+print(groceries)
