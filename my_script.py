0ec31e423d3e4573ad318323f27d3ec6ad91390b
Prem Kumar <codeon.prem@gmail.com>
2025-11-22 15:11:50 -0500

    added changes

---
+++ my_script.py
@@ -0,0 +1,10 @@
+# my_list = [1,2,"apple",4.5,"tomato",75]
+# a_list = ["these","are","elements"]
+# first_three = my_list[0:3]
+# print(first_three)
+# my_list2 = [0,1,2,3,4,5]
+# print(my_list2[::-1])
+
+my_shopping_list = ["bread","milk","eggs"]
+extend = my_shopping_list.extend(["spinach","ghee"])
+print(extend)
